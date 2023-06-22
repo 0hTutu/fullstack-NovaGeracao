80a80367db33c64ffa062e31c0d422cbafaf3040
Arthur Müller <112774071+0hTutu@users.noreply.github.com>
2023-06-22 16:09:07 -0300

    Add files via upload

---
+++ Aula5/arrayVazia.js
@@ -0,0 +1,5 @@
+let arrayvazia = [,,,];
+console.log(arrayvazia.length);
+arrayvazia.push(50)
+console.log(arrayvazia);
+console.log(arrayvazia.length);
---
+++ Aula5/atualizando-chamada.js
@@ -0,0 +1,5 @@
+const alunos = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+
+alunos.splice(1, 2, "Rodrigo");
+
+console.log(alunos);
