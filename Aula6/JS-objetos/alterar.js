77fc2ce12f7c49b3021395d8fa496467d51dca29
Arthur Müller <112774071+0hTutu@users.noreply.github.com>
2023-07-01 17:10:58 -0300

    alteração de objeto

--- Aula6/JS-objetos/alterar.js
+++ Aula6/JS-objetos/alterar.js
@@ -2,3 +2,14 @@ const pessoa = {
     nome: "Jorge",
     profissao: "borracheiro"
 }
+
+console.log(pessoa.nome);
+console.log(pessoa.telefone);
+
+pessoa.telefone = "51 997289043";
+
+console.log(pessoa.telefone);
+
+pessoa.nome = "Jorge Silveira";
+
+console.log(pessoa);
