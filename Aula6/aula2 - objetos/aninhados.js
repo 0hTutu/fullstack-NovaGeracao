bcb5b1ded693f0d58ff177b77219424a5198a401
Arthur Müller <112774071+0hTutu@users.noreply.github.com>
2023-07-02 18:56:47 -0300

    objetos aninhados

---
+++ Aula6/aula2 - objetos/aninhados.js
@@ -0,0 +1,15 @@
+const cliente = {
+    nome = "José",
+    idade = 24,
+    email = "josesilva@firma.com",
+    telefone = ["51 99232307","51 99132340" ]
+};
+
+cliente.endereco = {
+    rua = " Av. Franlin Roosevelt",
+    numero = "732",
+    ap = false,
+    complemento = "Casa verde"
+};
+
+console.log(cliente.endereco);
