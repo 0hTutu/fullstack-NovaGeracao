3a6cc2e6be132effa300ba6226f5a3f0a9dd3667
Arthur Müller <112774071+0hTutu@users.noreply.github.com>
2023-07-02 23:03:43 -0300

    mudança de JSON

--- Aula6/Aula4 - JSON/leitura.js
+++ Aula6/Aula4 - JSON/leitura.js
@@ -2,3 +2,15 @@ const dados = require("./cliente.json")
 
 console.log(dados);
 console.log(typeof dados);
+
+const clienteEmString = JSON.stringify(dados);
+
+console.log(clienteEmString);
+console.log(typeof clienteEmString);
+
+//vai ser undefined
+console.log(clienteEmString.nome);
+
+//transformar novamente em object
+const objetoCliente = JSON.parse(clienteEmString);
+console.log(objetoCliente);
