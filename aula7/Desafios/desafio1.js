ffcfbec64bcac09aab71dd52fc354be6264ccd55
Arthur Müller <112774071+0hTutu@users.noreply.github.com>
2023-07-04 16:35:15 -0300

    Create desafio1.js

---
+++ aula7/Desafios/desafio1.js
@@ -0,0 +1,11 @@
+/*
+Como fazer:
+
+Abra o inspecionar em qualquer página do navegador;
+Na aba “console” do inspecionar passe a lista de cores;
+Aplique o método para remover a cor “Vermelho”;
+Utilize console.log(lista); ao final para conferir o resultado.
+*/
+
+var lista = ["Laranja", "Vermelho", "Branco", "Amarelo", "Rosa"]; 
+
