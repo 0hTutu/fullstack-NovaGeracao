d101fe810a02901dbf2278d0a8ce68d4ea47c3be
Arthur Müller <112774071+0hTutu@users.noreply.github.com>
2023-07-12 15:04:22 -0300

    Add files via upload

---
+++ aula8/PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+public class PrimeiroPrograma{
+    
+    public static void main(String[] args) {  
+        
+        System.out.println("ola!");
+        System.out.println("tudo bem?");
+        System.out.println("como anda voce?");
+    } 
+}
