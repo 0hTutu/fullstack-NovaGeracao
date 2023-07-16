e71ee649fc0f353bc5c6efa6c6b344bf8a7df155
Arthur Müller <112774071+0hTutu@users.noreply.github.com>
2023-07-16 17:38:54 -0300

    Create TesteSomatoria.java

---
+++ aula8/sintaxe-variaveis-e-fluxo/TesteSomatoria.java
@@ -0,0 +1,14 @@
+package sintaxevariaveisefluxo;
+
+public class TesteSomatoria {
+	
+	public static void main(String[] args) {
+		int i = 0;
+		int soma = 0;
+		while (i <= 10) {
+			soma += i;
+			i++;
+		}
+		System.out.println(soma);
+	}
+}
