4fd587d78d1b6361489a02b024c6417aa626f00b
Arthur Müller <112774071+0hTutu@users.noreply.github.com>
2023-07-17 21:30:32 -0300

    Create Conta.java

---
+++ aula8/ProBank/src/Conta.java
@@ -0,0 +1,16 @@
+package ProBank;
+
+public class Conta {
+	double saldo;
+	int agencia;
+	int numero;
+	String titular;
+	
+	public void deposita(double valor){
+		this.saldo += valor;
+	}
+	
+	public void saca() {
+		
+	}
+}
