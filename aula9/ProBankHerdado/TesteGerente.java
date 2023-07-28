07b9dfbabe68f5393b6dcb996bf1deb756cc93e1
Arthur Müller <112774071+0hTutu@users.noreply.github.com>
2023-07-27 22:28:51 -0300

    gerente

---
+++ aula9/ProBankHerdado/TesteGerente.java
@@ -0,0 +1,11 @@
+package ProBankHerdado;
+
+public class TesteGerente{
+
+    public static void main(String[] args){
+
+        Gerente g1 = new Gerente();
+        g1.setNome("");
+        g1.setCpf("")
+    }
+}
